f52485af683832bb699550f9c57de0e4a4ec6929
zdcaryy <853654611@qq.com>
2019-05-27 11:36:42 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
 import Vue from 'vue';
-console.log('ok');
+
+import router from './router.js';
+
+import app from './App.vue';
+
+//按需导入 mintui
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+//引入mui
+import './lib/mui/css/mui.min.css';
+
+var vm=new Vue({
+    el:'#app',
+    render:c=>c(app),
+    router
+})
